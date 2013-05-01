320ec57d0e6ecb7e9da8689ae89cdc544020ed54
sawyerbernath <sawyerbernath@gmail.com>
2013-05-01 01:21:05 -0600

    Rubified TimeClock API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,25 @@ class ApplicationController < ActionController::Base
       redirect_to '/home'
     end  
   end
+
+  API_URL = "http://10.1.10.78/timeclock/timeclock_api.php"
+  def punch_in (username, password)
+    response = HTTParty.get("#{API_URL}?operation=100&username=#{username}&password=#{password}")
+    #check for failure?
+  end
+
+   def punch_out (username, password)
+    response = HTTParty.get("#{API_URL}?operation=101&username=#{username}&password=#{password}")
+     #check for failure?
+  end
+
+   def fetch_elves
+     response =  HTTParty.get("#{API_URL}?operation=102")
+     JSON.parse(response.parsed_response)
+   end
+
+   def fetch_in_out (username)
+     response = HTTParty.get("#{API_URL}?operation=103&username=#{username}")
+     JSON.parse(response.parsed_response)
+   end
 end
